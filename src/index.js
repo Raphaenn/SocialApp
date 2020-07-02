dc406281d622fa183899432799e4396e6481e728
Raphael Neves <raphaelneves@MacBook-Air-de-Raphael.local>
2020-07-02 17:46:23 -0300

    codepush and pushNotifications done

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import React, { Component } from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { StatusBar } from "react-native";
+import CodePush from "react-native-code-push";
+import OneSignal from "react-native-onesignal";
 
 import "./config/reactotronConfig";
 import { store, persistor } from "./store";
 import App from "./app";
 
-export default class Index extends Component {
+class Index extends Component {
     constructor(props) {
         super(props);
+        OneSignal.init("8a5c7f1e-e9e3-4fd7-90a3-bc64c664acfa");
+        OneSignal.addEventListener('received', this.onReceived);
+        OneSignal.addEventListener('opened', this.onOpened);
+        OneSignal.addEventListener('ids', this.onIds);
     }
+
+    componentDidMount() {
+        OneSignal.removeEventListener('received', this.onReceived);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        OneSignal.removeEventListener('ids', this.onIds)
+    }
+
+    onReceived = (data) => {};
+
+    onOpened = (notification) => {};
+
+    onIds = (id) => {};
 
     render() {
         return (
@@ -21,7 +39,10 @@ export default class Index extends Component {
                 </PersistGate>
             </Provider>
         )
-
     }
 }
+
+export default CodePush({
+    checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index)
 
