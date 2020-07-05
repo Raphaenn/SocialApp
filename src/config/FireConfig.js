8b2031ca8646e2d155097cc16694a6deedd5ab79
Raphael Neves <38678724+Raphaenn@users.noreply.github.com>
2020-07-05 15:53:56 -0300

    firebase keys

--- src/config/FireConfig.js
+++ src/config/FireConfig.js
@@ -1,10 +1,10 @@
 export default FirebaseKeys = {
-    apiKey: "AIzaSyBGA2hE6ktno7VTTgJGZQl36LCUEhXXfhI",
-    authDomain: "socialapp-865c1.firebaseapp.com",
-    databaseURL: "https://socialapp-865c1.firebaseio.com",
-    projectId: "socialapp-865c1",
-    storageBucket: "socialapp-865c1.appspot.com",
-    messagingSenderId: "98616151967",
-    appId: "1:98616151967:web:137f176e3fccc7ebf2b7aa",
-    measurementId: "G-WG7VGNLL1P"
+    apiKey: "your-Data",
+    authDomain: "your-Data",
+    databaseURL: "your-Data",
+    projectId: "your-Data",
+    storageBucket: "your-Data",
+    messagingSenderId: "your-Data",
+    appId: "your-Data",
+    measurementId: "your-Data"
 }
